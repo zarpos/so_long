b927ca4ccfe68ce7ef4e398736e784644e7ff4cd
Diego Rubio morato <drubio-m@c1r17s1.42madrid.com>
2023-02-02 16:28:37 +0100

    ;)

---
+++ mlx_test.c
@@ -0,0 +1,35 @@
+
+// -------------------------------------DOCUMENTATION------------------------------------------------ //
+
+// To compile the minilibx use clang main.c -lX11 -lXext -lmlx
+
+// Returns a (void *) to the beginning of the window, so whe should create a variable to store the return vaule and use it as the *mlx_ptr
+void *mlx_init();
+
+// Here we open our first window;
+void *mlx_new_window(void *mlx_ptr, int WINDOW_WIDTH, int WINDOW_HEIGHT, char *title);
+
+// Returns a void * to use it as reference to modify the image later ;) 
+void *mlx_new_image(void *mlx_ptr, int width, int height);
+
+// 
+char *mlx_get_data_addr(void *img_ptr, int *bits_per_pixel, int *size_line, int *endian);
+
+//
+int mlx_put_image_to_window(void *mlx_ptr, void *win_ptr, void *img_ptr, int x, int y);
+
+//
+unsigned int mlx_get_color_value(void *mlx_ptr, int color);
+
+//
+void *mlx_xpm_to_image(void *mlx_ptr, char **xpm_data, int *width, int *height);
+
+//
+void *mlx_xpm_file_to_image(void *mlx_ptr, char *filename, int *width, int *height);
+
+// As the name says, it destroys the window.
+void *mlx_destroy_window(void *mlx_ptr, void *win_ptr);
+
+// This function terminates to free all the program, as the display opened before is the last element using memory.
+void *mlx_destroy_display(void *mlx_ptr);
+// ------------------------------------------------------------------------------------------------- //
--- prueba.c
+++ prueba.c
@@ -1,46 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   prueba.c                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: drubio-m <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/18 13:46:42 by drubio-m          #+#    #+#             */
-/*   Updated: 2022/05/18 13:54:06 by drubio-m         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
+#include <stdlib.h>
 #include <mlx.h>
 
-typedef struct s_data {
-    void	*img;
-    char	*addr;
-    int		bits_per_pixel;
-    int		line_length;
-    int		endian;
-}   t_data;
-
-
-void my_mlx_pixel_put(t_data *data, int x, int y, int color)
+int main()
 {
-char *dst;
+	void *mlx_ptr;
+	void *win_ptr;
 
-dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-*(unsigned int*)dst = color;
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 1920, 1080, "hola");
+	mlx_destroy_window(mlx_ptr, win_ptr);
+//	mlx_destroy_display(mlx_ptr);
+	free(mlx_ptr);
 }
-
-int main(void)
-{
-	void	*mlx;
-	void	*mlx_win;
-    t_data  img;
-
-    mlx = mlx_init();
-    mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
-    img.img = mlx_new_image(mlx, 1920, 1080);
-    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
-    my_mlx_pixel_put(&img, 5, 5, 0x00FF0000);
-    mlx_put_image_to_window(mlx, mlx_win, img.img, 5, 5);
-    mlx_loop(mlx);
-}
- 
